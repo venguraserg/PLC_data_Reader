0637fca7e82b18de6e2e1e39d27a5c3ba90cf69d
venguraserg <venguraserg@gmail.com>
2023-12-22 11:55:41 +0300

    init: add new console proj

---
+++ PLC_DATA_READER_CONSOLE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PLC_DATA_READER_CONSOLE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
