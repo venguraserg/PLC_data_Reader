f0eff7b5947adc64de70c5b5757997513f89ec5c
venguraserg <venguraserg@gmail.com>
2023-12-22 12:41:15 +0300

    Init: Create console App

---
+++ PLC_Reader_Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PLC_Reader_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
